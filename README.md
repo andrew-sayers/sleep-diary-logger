6172fb131c2e1333d089295543866e5059af7b5b
Andrew Sayers <andrew-github.com@pileofstuff.org>
2020-11-17 10:17:07 +0000

    Initial version

---
+++ README.md
@@ -0,0 +1,26 @@
+# Sleep Diary Toolkit - Logger
+
+Simple logger, designed for use first thing in the morning and last thing at night.
+
+* when you go to bed, press the _moon_ icon
+* when you wake up, press the _sun_ icon
+* when you want more options, press the _menu_ link
+* if you have a target wake time, scroll down for suggestions
+
+## License
+
+Simple logger, designed for use first thing in the morning and last thing at night.
+Copyright (C) 2020  Andrew Sayers (andrew-github.com@pileofstuff.org)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
---
+++ index.js
@@ -0,0 +1,73 @@
+/**
+ * @file Log sleep diary events
+ * @author Andrew Sayers (andrew-github.com@pileofstuff.org)
+ * @copyright 2020
+ * @license GPLv2
+ */
+
+browser_utils.fix_browser_issues();
+
+var WAKE_time   = document.getElementById('WAKE-time'),
+    SLEEP_time = document.getElementById('SLEEP-time'),
+    times      = document.getElementById('guidance'),
+    time_list  = document.getElementById('time-list'),
+    class_prefix = ''
+;
+
+function update(event) {
+
+    var diary = new Diary(),
+        analysed_diary = diary.analyse(),
+        latest_sleep = analysed_diary.sleeps[analysed_diary.sleeps.length-1],
+        now = luxon.DateTime.local()
+    ;
+
+    // handle clicks on the icons:
+    if ( event ) {
+        diary.add_entry(event.target.id);
+    }
+
+    // update event times:
+    if ( analysed_diary.sleeps.length ) {
+        SLEEP_time.innerHTML = now.diff(luxon.DateTime.fromMillis(latest_sleep.estimated_sleep_time)).toFormat('hh:mm') + ' ago';
+        WAKE_time.innerHTML  = now.diff(luxon.DateTime.fromMillis(latest_sleep.estimated_wake_time)).toFormat('hh:mm') + ' ago';
+    }
+
+    if ( analysed_diary.suggested_day_lengths ) {
+        times.removeAttribute('style');
+        time_list.innerHTML
+            = analysed_diary.suggested_day_lengths
+            .map(function(suggestion) {
+                return (
+                    '<tr><td>' + luxon.DateTime.fromMillis(suggestion.bed_time).toFormat('HH:mm') +
+                        '<td>' +                           suggestion.days_remaining +
+                        '<td>' + luxon.DateTime.fromMillis(suggestion.day_length).toFormat('HH:mm') +
+                    '</tr>'
+                );
+            }).join('');
+    } else {
+        times.setAttribute('style','display:none');
+    }
+
+    // Set page mode:
+    var mode = diary.mode();
+    if ( !isNaN(mode) ) {
+        document.body.className = class_prefix + diary.event_id_to_string[mode];
+    }
+    class_prefix = 'transition ';
+
+    // So that queued updates are sent when we go online:
+    diary.save();
+
+}
+
+browser_utils.fix_browser_issues();
+
+document.getElementById('WAKE' ).addEventListener( 'click', update );
+document.getElementById('SLEEP').addEventListener( 'click', update );
+
+document.addEventListener( "visibilitychange", function() {
+    if ( document.visibilityState == 'visible' ) update();
+});
+setInterval(update, 60000);
+update();
